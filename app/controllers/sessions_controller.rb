c7be1f6e203ffa7d3ed7cee1c20144a5c0fa9f8b
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2022-11-09 01:18:35 +0800

    Added Dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to (session[:intended_url] || user)
+      redirect_to users_path
       session[:intended_url] = nil
     else
       render :new, status: :unprocessable_entity
