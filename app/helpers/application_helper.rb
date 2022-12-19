f5f4363c93da4e47464629cdf287a30a7210669b
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2022-12-19 21:01:09 +0800

    Fix Double Render Issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def controller_stylesheet_link_tag
-		stylesheet_link_tag params[:controller], "data-turbo-track": "reload"
+		stylesheet_link_tag params[:controller]
 	end
 end
