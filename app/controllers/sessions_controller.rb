4f7ea2528f1aa74a4eee8c677a3d7219899b7ba4
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2022-12-21 14:07:30 +0800

    Added Login form errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
     if current_user
       redirect_to users_path
     end
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      log_in user
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      log_in @user
       redirect_to (session[:intended_url] || users_path), flash: { notice: "Successully Logged In" }
     else
-      render :new, status: :unprocessable_entity, flash: { alert: "Invalid Username or Password" }
+      flash.now[:alert]= "Invalid Username/Password"
+      render :new, status: :unprocessable_entity
     end
   end
 
