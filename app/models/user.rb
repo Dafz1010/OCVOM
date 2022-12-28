4afffed90f76c413dd03f559f3a4ec0d27a1e875
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2022-12-28 09:29:39 +0800

    Add User Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_paper_trail
   has_secure_password
+
+  validates :name, presence: true
+  validates :username, presence: true, :uniqueness => { case_sensitive: false }
+  validates :password, presence: true, confirmation: true
+  validates :role_id, presence: true, allow_blank: true
 end
