81e691796e9992ae474732bc09821ede5d3623c3
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2022-12-28 09:30:55 +0800

    Set Session expiry to 24 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_ocvom_session', expire_after: 15.seconds
+Rails.application.config.session_store :cookie_store, key: '_ocvom_session', expire_after: 24.hours
