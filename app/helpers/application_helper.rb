ef07b959c02389d18a298dce6b8a7a20a12228ba
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-01-09 21:10:52 +0800

    Fix blank profile pic on staging
    This reverts commit d5cd289fcc3a051cfeb4c980362f327493314ae9.
    
    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
 	end
 
 	def has_profile_image
-		"style=background-image:url(#{current_user.profile_image_url})" if current_user.profile_image.present?
+		"style=background-image:url(data:#{current_user.profile_image.content_type};base64,#{Base64.strict_encode64(current_user.profile_image.read)})" if current_user.profile_image.present?
 	end
 end
