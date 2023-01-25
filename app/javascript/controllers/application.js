7165f90f37d90ba3acc55dda3e6b8ac5d4014b24
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-01-25 16:52:49 +0800

    fix select2 error

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,33 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import $ from 'jquery'
-import 'select2';
-window.$ = $;
-window.jQuery = $;
 const application = Application.start();
 
-function matchStart(params, data) {
-    if ($.trim(params.term) === '') {
-        return data;
-    }
-    if (typeof data.children === 'undefined') {
-        return null;
-    }
-    var filteredChildren = [];
-    $.each(data.children, function (idx, child) {
-        if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0) {
-            filteredChildren.push(child);
-        }
-    });
-    if (filteredChildren.length) {
-        var modifiedData = $.extend({}, data, true);
-        modifiedData.children = filteredChildren;
-        return modifiedData;
-    }
-    return null;
-    }
-$(".select2").select2({
-    matcher: matchStart
-});
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
