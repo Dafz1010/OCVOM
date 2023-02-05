111dfac33b3e51b38a2c23fac4b598e0ea1744b6
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-02-06 04:08:39 +0800

    View pdf instead of download

--- app/controllers/data_export_controller.rb
+++ app/controllers/data_export_controller.rb
@@ -1,10 +1,11 @@
 class DataExportController < ApplicationController
+  include Authentication
   def index
     @exportable_list = ["Dogs","Users","Logs"]
     @date_range = ["Today", "Yesterday", "Last 3 days", "Last Week","Last Month"]
   end
   def download_report
     pdf = Dog.export
-    send_data pdf.render, filename: "Dog_report.pdf", type: "application/pdf", disposition: "attachment"
+    send_data pdf.render, filename: "Dog_report.pdf", type: "application/pdf", disposition: "inline"
   end
 end
