4b22f5a98f2d37e918f43604af0c0e0721cfbd10
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-03-06 11:17:21 +0800

    localize all http requests

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "select2" # @4.1.0
-pin "jquery" # @3.6.3
+pin "bootstrap"
