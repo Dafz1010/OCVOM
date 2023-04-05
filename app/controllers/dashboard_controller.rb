c3f6bce099a8f166bbccf5d6ea64da38474f4f13
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-04-05 22:47:48 +0800

    Drying Code DashboardController

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,47 +10,55 @@ class DashboardController < ApplicationController
 
   def sort_logs(unsorted_logs)
     unsorted_logs.map do |record|
+      localtime = record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')
+      who_did_it = record.whodunnit
       case record[:event]
         when 'Login User'
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} Logged In"}
+          {log_text: "#{localtime}: User #{who_did_it} Logged In"}
         when 'Logout User'
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} Logged Out"}
+          {log_text: "#{localtime}: User #{who_did_it} Logged Out"}
         when 'Create User'
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} was Created by Admin"}
+          {log_text: "#{localtime}: User #{who_did_it} created User \"#{get_user_data(record.item_id).username}\""}
+        when 'User Restore'
+          {log_text: "#{localtime}: User #{who_did_it} restored User \"#{get_user_data(record.item_id).username}\""}
         when 'Archive User'
-          user_data = User.find(record.item_id)
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} archived User \"#{user_data.username}\""}
+          {log_text: "#{localtime}: User #{who_did_it} archived User \"#{get_user_data(record.item_id).username}\""}
         when 'Approve User'
-          user_data = User.find(record.item_id)
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} approve User \"#{user_data.username}\""}
+          {log_text: "#{localtime}: User #{who_did_it} approve User \"#{get_user_data(record.item_id).username}\""}
         when -> (tmp) { tmp.starts_with?("Set User Role") }
-          user_data = User.find(record.item_id)
           role = record[:event].split(",",2).last
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} set User \"#{user_data.username}\" role to \"#{role}\""}
+          {log_text: "#{localtime}: User #{who_did_it} set User \"#{get_user_data(record.item_id).username}\" role to \"#{role}\""}
         when 'Create Dog'
-          dog_data = Dog.find(record.item_id)
+          dog_data = get_dog_data(record.item_id)
           log_string = ""
           if dog_data.archived?
             log_string = "<div class='d-inline-block'>#{dog_data.uuid}</div>"
           else
             log_string = "<div class='d-inline-block'>#{link_to dog_data.uuid, dog_path(dog_data.uuid)}</div>"
           end
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} created Dog Profile ID: #{log_string}"}
+          {log_text: "#{localtime}: User #{who_did_it} created Dog Profile ID: #{log_string}"}
         when 'Archive Dog'
-          dog_data = Dog.find(record.item_id)
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} archived Dog Profile ID: <div class='d-inline-block'>#{dog_data.uuid}</div>"}
+          {log_text: "#{localtime}: User #{who_did_it} archived Dog Profile ID: <div class='d-inline-block'>#{get_dog_data(record.item_id).uuid}</div>"}
         when 'Update Dog'
-          dog_data = Dog.find(record.item_id)
+          dog_data = get_dog_data(record.item_id)
           log_string = ""
           if dog_data.archived?
             log_string = "<div class='d-inline-block'>#{dog_data.uuid}</div>"
           else
             log_string = "<div class='d-inline-block'>#{link_to dog_data.uuid, dog_path(dog_data.uuid)}</div>"
           end
-          {log_text: "#{record.created_at.localtime.strftime('%b %d, %Y %I:%M %p')}: User #{record.whodunnit} updated Dog Profile ID: #{log_string}"}
+          {log_text: "#{localtime}: User #{who_did_it} updated Dog Profile ID: #{log_string}"}
         else 
           {log_text: "Event: #{record[:event]}"}
       end
     end
+  end
+
+  def get_user_data(user_id)
+    User.find(user_id)
+  end
+
+  def get_dog_data(dog_id)
+    Dog.find(dog_id)
   end
 end
