1371bfedfcc6ba8e44147d4eb58f504f33a5a403
Dafz1010 <115939063+Dafz1010@users.noreply.github.com>
2023-05-13 02:04:57 +0800

    refactor data_export

--- app/helpers/data_export_helper.rb
+++ app/helpers/data_export_helper.rb
@@ -1,5 +1,5 @@
 module DataExportHelper
-    def default_date_range
-        "#{(Date.today - 7).strftime("%b %d, %Y")} - #{Date.today.strftime("%b %d, %Y")}"
-    end
+    # def default_date_range
+    #     "#{(Date.today - 7).strftime("%b %d, %Y")} - #{Date.today.strftime("%b %d, %Y")}"
+    # end
 end
